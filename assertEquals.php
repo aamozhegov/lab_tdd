ddeae4132c6b282987baa199a4c13f0e3d08053b
Artem Mozhegov <amozhegov@yahoo.com>
2020-11-11 18:41:01 +0300

    Написал свою функцию проверки соответствия ожидаемых и реальных результатов

--- assertEquals.php
+++ assertEquals.php
@@ -2,28 +2,34 @@
 
 require_once('./readFromConsole.php');
 
-function assertEquals($expected, $received, $message = '') {
-
-	$result = readFromConsole(true, true);
-	echo 'Тест '. ($result === $expected ? 'пройден' : 'не пройден').PHP_EOL;
-
-	$result = readFromConsole(false, false);
-	echo 'Тест '. ($result === $expected ? 'пройден' : 'не пройден').PHP_EOL;
-
-	$result = readFromConsole(null);
-	echo 'Тест '. ($result === $expected ? 'пройден' : 'не пройден').PHP_EOL;
-
+/*
+ *readFromConsole('', 'true') - true;
+ *readFromConsole('', 'false') - false;
+ *readFromConsole('', '!stop') - null;
+ *readFromConsole('', '1.3') - 1.3;
+ *readFromConsole('', '1') - 1;
+ *readFromConsole('', 'test') = 'test;
+ */
+function assertEquals($expected, $received, $message='') {
+	$result = readFromConsole(true);
+	assertEquals(true, $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === true ? 'тест пройден':'тест не пройден').PHP_EOL;
+	$result = readFromConsole(false);
+	assertEquals(false, $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === false ? 'тест пройден':'тест не пройден').PHP_EOL;
+	$result = readFromConsole('!stop');
+	assertEquals(null, $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === null ? 'тест пройден':'тест не пройден').PHP_EOL;
 	$result = readFromConsole(1.3);
-	echo 'Тест '. ($result === 1.3 ? 'пройден' : 'не пройден').PHP_EOL;
-
+	assertEquals(1.3, $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === 1.3 ? 'тест пройден':'тест не пройден').PHP_EOL;
 	$result = readFromConsole(1);
-	echo 'Тест '. ($result === 1 ? 'пройден' : 'не пройден').PHP_EOL;
-
+	assertEquals(1, $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === 1 ? 'тест пройден':'тест не пройден').PHP_EOL;
 	$result = readFromConsole('test');
-	echo 'Тест '. ($result === 'test' ? 'пройден' : 'не пройден').PHP_EOL;
+	assertEquals('test', $result, 'Результат теста: ').PHP_EOL;
+	//echo 'Результат: '. $result . ($result === 'test' ? 'тест пройден':'тест не пройден').PHP_EOL;
 }
 
-/*
-$result = calculate(1, 2, '+');
-assertEquals(3, $result, 'Test: 1 + 2 = 3 ');
-*/
+test_readFromConsole(); 
+echo test_readFromConsole(); 
